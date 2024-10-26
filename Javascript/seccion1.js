9b2ead03156f68514d020c6639163656019c7318
fbarradas27 <barradasfranklin7@gmail.com>
2024-10-26 11:40:26 -0400

    update 14

---
+++ Javascript/seccion1.js
@@ -0,0 +1,21 @@
+alert("Ma ejecuto desde un archivo extero");
+
+//Comentario de una sola linea
+/*
+  Comentario de
+  varias lineas
+*/
+//La consola
+console.log("Hola Mundo");
+
+//VARIABLES
+//inicializacion de una variable
+var edad= 5;
+//Utilizacion de una variable
+console.log(edad);
+
+//TIPOS DE DATOS
+var exponente = 5 ** 2;
+console.log(exponente);
+
+
